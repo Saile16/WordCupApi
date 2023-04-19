318c8afd61dd09fd76871bb74465896d36c6ca0b
Alexander <alrlucano@gmail.com>
2023-04-19 12:12:18 -0500

    Agregado paginas de inicio , pocos estilos

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 import Layout from "@/components/Layout";
-import TablaGrupo from "@/components/TablaGrupo";
-import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-
-export default function Home({ responseMatches }) {
+import styles from "../styles/Home.module.css";
+export default function Home() {
   return (
     <Layout>
-      <div className="container">
-        <h1>Bienvenido a WorldCupAPI</h1>
-        <ul>
-          <li>
-            <Link href="">Ver Grupos</Link>
+      <div className={`container ${styles["home-container"]}`}>
+        <h1 className="">Bienvenido a WorldCupAPI</h1>
+        <ul className={styles["container-list"]}>
+          <li className={styles["item-list"]}>
+            <Link className={styles.item} href="/grupos">
+              Ver Grupos
+            </Link>
           </li>
-          <li>
-            <Link href="">Resultados Completos</Link>
+          <li className={styles["item-list"]}>
+            <Link className={styles.item} href="/resultados">
+              Resultados Completos
+            </Link>
           </li>
-          <li>
+          {/* <li className={styles["item-list"]}>
             <Link href=""></Link>
-          </li>
+          </li> */}
         </ul>
       </div>
     </Layout>
